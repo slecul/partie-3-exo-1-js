1b6a6d91b6082a714bb0f071cf7b30b6ffb8a414
slecul <slecul@laposte.net>
2019-01-28 11:35:15 +0100

    v1

---
+++ assets/js/exo1js.js
@@ -0,0 +1,10 @@
+function addborder(){
+  var image = document.getElementById('image1');
+
+  image.setAttribute('class', 'border');
+}
+function delborder(){
+  var image = document.getElementById('image1');
+
+  image.setAttribute('class', '');
+}
